95b4ba0261a898527850721e44015aded67ff324
tsvetkovHomeVsCode <dstvetkov@gmail.com>
2023-09-07 13:15:49 +0300

    прошел 1.6 и начал 1 .7

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import Counter from "./components/Counter";
-import './index.scss'
+import { Suspense } from 'react'
+import './styles/index.scss'
+import { Route, Routes, Link } from 'react-router-dom'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 
 const App = () => {
-    return (
-        <div className={'app'}>
-            asdasdasdas
-            <Counter />
-        </div>
-    );
-};
+  return (
+    <div className={'app'}>
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />}></Route>
+          <Route path="/about" element={<AboutPageAsync />}></Route>
+        </Routes>
+      </Suspense>
+    </div>
+  )
+}
 
-export default App;
+export default App
