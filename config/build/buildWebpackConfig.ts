300519543c04c88ab0f51feed39d5db7154690ac
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2023-09-09 21:34:35 +0300

    Модуль 1.9 и 1.10 - поменял структуру проекта на FSD

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,5 @@
-import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import webpack from "webpack";
 import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
@@ -7,10 +7,10 @@ import {buildResolvers} from "./buildResolvers";
 import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {paths, mode, isDev} = options
+    const {paths, mode, isDev} = options;
 
     return {
-        mode: mode,
+        mode,
         entry: paths.entry,
         output: {
             filename: "[name].[contenthash].js",
@@ -19,10 +19,10 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         plugins: buildPlugins(options),
         module: {
-            rules: buildLoaders(options),
+            rules: buildLoaders(options)
         },
-        resolve: buildResolvers(),
+        resolve: buildResolvers(options),
         devtool: isDev ? 'inline-source-map' : undefined,
-        devServer: isDev ? buildDevServer(options) : undefined
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
