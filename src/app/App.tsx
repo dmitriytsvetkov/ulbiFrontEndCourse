5667457d2c10d2417f79dd0287a1f4a86cb75d17
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2023-09-16 17:04:44 +0300

    WIP

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './styles/index.scss';
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-
+import React from 'react'
+import './styles/index.scss'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useTheme } from 'app/providers/ThemeProvider'
+import { AppRouter } from 'app/providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
-    const {theme} = useTheme();
+    const { theme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
+            <Navbar />
             <AppRouter></AppRouter>
         </div>
-    );
-};
+    )
+}
 
-export default App;
+export default App
