c2fc9aa97e78cc911983ac89a411bcc68e36ee10
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2023-09-20 22:12:55 +0300

    2.4 Добавил сайдбар

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,21 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
     return (
-        <div>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    {Object.values(routeConfig).map(({element, path}) => (
-                        <Route
-                            key={path}
-                            path={path}
-                            element={element}
-                        />
-                    ))}
-                </Routes>
-            </Suspense>
-        </div>
-    );
-};
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={<div className="page-wrapper">{element}</div>}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    )
+}
 
-export default AppRouter;
+export default AppRouter
