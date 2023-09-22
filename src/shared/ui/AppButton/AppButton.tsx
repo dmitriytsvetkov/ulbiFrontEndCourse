a97f8bf62ae8c6db5a1670a55a079d617490e8db
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2023-09-22 23:13:44 +0300

    2.5 Добавил модуль перевода

--- src/shared/ui/AppButton/AppButton.tsx
+++ src/shared/ui/AppButton/AppButton.tsx
@@ -1,21 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import cls from './Button.module.scss'
+import cls from './AppButton.module.scss'
 import { ButtonHTMLAttributes, FC } from 'react'
 
 export enum ThemeButton {
     CLEAR = 'clear',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string
     theme?: string
 }
 
-export const Button: FC<ButtonProps> = (props) => {
+export const AppButton: FC<AppButtonProps> = (props) => {
     const { children, className, theme, ...otherProps } = props
     return (
         <button
-            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            className={classNames(cls.AppButton, {}, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Sidebar.module.scss'
 import { useState } from 'react'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
+import { LangSwitcher } from 'shared/ui/LangSwitcher/ui/LangSwitcher'
 
 interface SidebarProps {
     className?: string
@@ -22,6 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             <button onClick={onToggle}>Toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     )
