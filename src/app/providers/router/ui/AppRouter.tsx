ece65937278a74dcdc226859b608f9687c313166
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2023-10-16 12:26:40 +0300

    21. Добавил страницу 404, добавил лоадер

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 const AppRouter = () => (
     <Suspense fallback={<div>Loading...</div>}>
@@ -12,7 +13,7 @@ const AppRouter = () => (
                     key={path}
                     path={path}
                     element={(
-                        <Suspense fallback={<div>Loading...</div>}>
+                        <Suspense fallback={<PageLoader />}>
                             <div className="page-wrapper">
                                 {element}
                             </div>
