7e711fe0027fa620866cab4c35db05d2de4a0fde
tsvetkovDmitriy <dmitriy.tsvetkov@best2pay.net>
2024-02-01 12:35:15 +0300

    41. Добавил модуль Профиля

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,9 +1,9 @@
-import path from 'path';
 import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
+import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: { config: webpack.Configuration }) => {
+export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
@@ -29,12 +29,10 @@ export default ({ config }: { config: webpack.Configuration }) => {
     });
     config!.module!.rules.push(buildCssLoader(true));
 
-    config!.plugins!.push(
-        new DefinePlugin({
-            __IS_DEV__: JSON.stringify(true),
-            __API__: JSON.stringify(''),
-        }),
-    );
+    config!.plugins!.push(new DefinePlugin({
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify(''),
+    }));
 
     return config;
 };
